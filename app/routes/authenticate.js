3a3d3e1a8d3c6e9013e97a019f3560f46bdd4b24
Peter Toth <tothpeter08@gmail.com>
2015-05-24 20:12:56 +0200

    Finish up auth

--- app/routes/authenticate.js
+++ app/routes/authenticate.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
+const { inject } = Ember;
 
 export default Ember.Route.extend({
+  firebase: inject.service(),
+
+  model() {
+    return {
+      credentials: {}
+    };
+  },
+
+  actions: {
+    authenticate() {
+      const firebase = this.get('firebase');
+      const credentials = this.get('currentModel.credentials');
+
+      this.set('currentModel.isLoading', true);
+      this.set('currentModel.error', null);
+
+      firebase.authWithPassword(credentials, error => {
+        this.set('currentModel.isLoading', false);
+
+        if (error) {
+          this.set('currentModel.error', error);
+        } else {
+          this.transitionTo('authenticated');
+        }
+      });
+    }
+  }
 });
--- tests/acceptance/authentication-test.js
+++ tests/acceptance/authentication-test.js
@@ -21,9 +21,41 @@ test('success', function(assert) {
   fillIn('input[type="email"]', 'tothpeter08@gmail.com');
   fillIn('input[type="password"]', '12345678');
   click('button[type="submit"]');
+  firebase.authWithPassword = ({ email, password}, callback) => {
+    assert.equal(email, 'tothpeter08@gmail.com');
+    assert.equal(password, '12345678');
+
+    const authData = { uid: 'test-data' };
+
+    firebase.changeAuthState(authData);
+    firebase.flush();
+
+    callback(null, authData);
+  };
 
   andThen(() => {
     assert.ok(find(':contains("Logged in")').length,
       'expected to see "Logged in"');
+  });
+});
+
+
+test('error', function(assert) {
+  visit('/');
+  fillIn('input[type="email"]', 'tothpeter08@gmail.com');
+  fillIn('input[type="password"]', '12345678');
+  click('button[type="submit"]');
+  firebase.authWithPassword = ({ email, password}, callback) => {
+    assert.equal(email, 'tothpeter08@gmail.com');
+    assert.equal(password, '12345678');
+
+    const error = new Error('Invalid password');
+
+    callback(error);
+  };
+
+  andThen(() => {
+    assert.ok(find(':contains("Invalid password")').length,
+      'expected to see "Invalid password"');
   });
 });
